974c88bb5d0d8d918e3992201658b5749248df35
Roman Miroshnychenko <roman.miroshnychenko@comeet.co>
2020-04-27 23:26:12 +0300

    Formatting

--- script.tvmaze/script.py
+++ script.tvmaze/script.py
@@ -13,5 +13,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 """Script entry point"""
+
 from __future__ import absolute_import, unicode_literals
--- script.tvmaze/service.py
+++ script.tvmaze/service.py
@@ -13,5 +13,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 """Service entry point"""
+
 from __future__ import absolute_import, unicode_literals
