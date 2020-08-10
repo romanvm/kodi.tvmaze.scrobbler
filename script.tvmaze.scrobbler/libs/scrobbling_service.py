ca2b526c5f319ee8aeff2d9233678e80c4cf7995
Roman Miroshnychenko <roman.miroshnychenko@comeet.co>
2020-08-10 16:46:04 +0300

    Use marked_at as Kodi lastplayed when pulling watched episodes from TVmaze

--- script.tvmaze.scrobbler/libs/scrobbling_service.py
+++ script.tvmaze.scrobbler/libs/scrobbling_service.py
@@ -31,6 +31,7 @@ from kodi_six import xbmc
 
 from . import gui, medialibrary_api as medialib, tvmaze_api as tvmaze, kodi_service as kodi
 from .kodi_service import logger
+from .time_utils import convert_to_time_string
 
 try:
     # pylint: disable=unused-import
@@ -251,7 +252,13 @@ def _pull_watched_episodes(kodi_tv_shows=None):
                     continue
                 if kodi_episodes:
                     kodi_episode_info = kodi_episodes[0]
-                    medialib.set_episode_playcount(kodi_episode_info['episodeid'])
+                    marked_at = episode.get('marked_at')
+                    if marked_at is not None:
+                        last_played = convert_to_time_string(marked_at)
+                    else:
+                        last_played = None
+                    medialib.set_episode_playcount(kodi_episode_info['episodeid'],
+                                                   last_played=last_played)
 
 
 def pull_watched_episodes():
