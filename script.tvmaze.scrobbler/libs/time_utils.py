96ec764dd9a79343fd3aa925f112d7fecb9b40b4
Roman Miroshnychenko <roman.miroshnychenko@comeet.co>
2020-08-10 16:52:52 +0300

    Log an error when unable to determine Kodi timezone

--- script.tvmaze.scrobbler/libs/time_utils.py
+++ script.tvmaze.scrobbler/libs/time_utils.py
@@ -5,6 +5,7 @@ import datetime
 
 import pytz
 
+from .kodi_service import logger
 from .medialibrary_api import get_kodi_timezone_string
 
 try:
@@ -19,7 +20,8 @@ def get_kodi_timezone():
     try:
         return pytz.timezone(kodi_timezone_string)
     except pytz.UnknownTimeZoneError:
-        pass
+        logger.error('Unable to determine Kodi timezone from string: "{}"'.format(
+            kodi_timezone_string))
     return None
 
 
