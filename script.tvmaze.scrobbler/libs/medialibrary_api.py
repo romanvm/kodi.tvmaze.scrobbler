a2a807fc6a18695d89e3d4e4d33d77173bd06336
Roman Miroshnychenko <roman.miroshnychenko@comeet.co>
2020-08-10 14:14:33 +0300

    Use Kodi dateadded as marked_at time for unwatched episodes

--- script.tvmaze.scrobbler/libs/medialibrary_api.py
+++ script.tvmaze.scrobbler/libs/medialibrary_api.py
@@ -147,7 +147,8 @@ def get_episode_details(episode_id):
     method = 'VideoLibrary.GetEpisodeDetails'
     params = {
         'episodeid': episode_id,
-        'properties': ['playcount', 'tvshowid', 'season', 'episode', 'uniqueid', 'lastplayed']
+        'properties': ['playcount', 'tvshowid', 'season', 'episode', 'uniqueid',
+                       'dateadded', 'lastplayed']
     }
     return send_json_rpc(method, params)['episodedetails']
 
--- script.tvmaze.scrobbler/libs/scrobbling_service.py
+++ script.tvmaze.scrobbler/libs/scrobbling_service.py
@@ -146,9 +146,9 @@ def _prepare_episode_list(kodi_episode_list):
     episodes_for_tvmaze = []
     for episode in kodi_episode_list:
         if episode['season']:  # Todo: add support for specials
-            last_played = episode.get('lastplayed')
-            if last_played:
-                timetuple = time.strptime(last_played, '%Y-%m-%d %H:%M:%S')
+            marked_at_sting = episode.get('lastplayed') or episode.get('dateadded')
+            if marked_at_sting:
+                timetuple = time.strptime(marked_at_sting, '%Y-%m-%d %H:%M:%S')
                 marked_at = int(time.mktime(timetuple))
             else:
                 marked_at = int(time.time())
