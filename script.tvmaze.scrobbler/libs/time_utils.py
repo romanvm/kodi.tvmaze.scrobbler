aaa9a9a0f62089fdd16d7ece5f2eaa0955e210bf
Roman <roman1972@gmail.com>
2020-08-28 15:34:27 +0300

    Use dateutil library to obtain user's timezone instead of a JSON-RPC call
    This fixes #19.

--- script.tvmaze.scrobbler/libs/time_utils.py
+++ script.tvmaze.scrobbler/libs/time_utils.py
@@ -4,18 +4,14 @@ from __future__ import absolute_import, unicode_literals
 import datetime
 import time
 
-import pytz
-
-from .kodi_service import logger
-from .medialibrary_api import send_json_rpc
+from dateutil import tz
 
 try:
-    from typing import Text, Optional  # pylint: disable=unused-import
+    from typing import Text  # pylint: disable=unused-import
 except ImportError:
     pass
 
 DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
-TIMEZONE = None
 
 
 class proxydt(datetime.datetime):  # pylint: disable=invalid-name
@@ -27,40 +23,21 @@ class proxydt(datetime.datetime):  # pylint: disable=invalid-name
 
     @classmethod
     def strptime(cls, date_string, format):  # pylint: disable=redefined-builtin
-        return datetime.datetime(*(time.strptime(date_string, format)[0:6]))
+        return datetime.datetime(*(time.strptime(date_string, format)[:6]))
 
 
 datetime.datetime = proxydt
 
 
-def _get_kodi_timezone():
-    # type: () -> Optional[pytz.tzinfo.DstTzInfo]
-    global TIMEZONE  # pylint: disable=global-statement
-    if TIMEZONE is None:
-        method = 'Settings.GetSettingValue'
-        params = {'setting': 'locale.timezone'}
-        result = send_json_rpc(method, params)
-        kodi_timezone = result.get('value')
-        try:
-            TIMEZONE = pytz.timezone(kodi_timezone)
-        except pytz.UnknownTimeZoneError:
-            logger.error('Unable to create a pytz timezone from Kodi timezone: "{}"'.format(
-                kodi_timezone))
-    return TIMEZONE
-
-
 def timestamp_to_time_string(posix_timestamp):
     # type: (int) -> Text
-    kodi_timezone = _get_kodi_timezone()
-    date_time = datetime.datetime.fromtimestamp(posix_timestamp, tz=kodi_timezone)
+    date_time = datetime.datetime.fromtimestamp(posix_timestamp, tz=tz.tzlocal())
     return date_time.strftime(DATETIME_FORMAT)
 
 
 def time_string_to_timestamp(time_string):
     # type: (Text) -> int
     time_object = datetime.datetime.strptime(time_string, DATETIME_FORMAT)
-    kodi_timezone = _get_kodi_timezone()
-    if kodi_timezone is not None:
-        time_object = kodi_timezone.localize(time_object)
+    time_object = time_object.replace(tzinfo=tz.tzlocal())
     timetuple = time_object.timetuple()
     return int(time.mktime(timetuple))
